ff463e7463ff4d3b56aa6c4c441850ec7c719075
Karan <karanpeshwani7@gmail.com>
2023-07-19 16:11:42 +0530

    Initial Commit

---
+++ VersionControlApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  VersionControlApp
+//
+//  Created by KARAN on 19/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.medium)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("hello Karan")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VersionControlApp/VersionControlAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersionControlAppApp.swift
+//  VersionControlApp
+//
+//  Created by KARAN on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct VersionControlAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
